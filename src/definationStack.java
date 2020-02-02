c0695330cec2a35fd9cadf03e0dfa414af6a8b65
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-02 22:22:53 +0530

    this method contain @param data of E type,@param index of int type. perform the adding operation in stack.

--- src/definationStack.java
+++ src/definationStack.java
@@ -1,8 +1,17 @@
+import NodeClass.Node;
 import STACKADT.StackInterface;
 
 public class definationStack<E> implements StackInterface<E> {
+    private Node<E> top;
+    private Node<E> bottom=null;
+    private int size=0;
+    public void add(E data, int size){
+
+    }
+
     @Override
     public void push(E data) {
+        add(data,size);
 
 
     }

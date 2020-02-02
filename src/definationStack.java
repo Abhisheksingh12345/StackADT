5d01935cbf52c707297d80b872b11bae07969ede
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-02 22:26:23 +0530

    this mehtod add firdt node in Stack.

--- src/definationStack.java
+++ src/definationStack.java
@@ -5,7 +5,19 @@ public class definationStack<E> implements StackInterface<E> {
     private Node<E> top;
     private Node<E> bottom=null;
     private int size=0;
+    public void addFirst(E data){
+        Node<E> node=new Node<>(data,null);
+        size++;
+    }
     public void add(E data, int size){
+        if(size<0) {
+            throw new IndexOutOfBoundsException();
+        }else if(size==0){
+            addFirst(data);
+        }else{
+
+        }
+
 
     }
 
